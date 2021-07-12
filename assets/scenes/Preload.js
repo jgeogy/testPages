58808ca75e1703e853cb7f792ee02ff4fc53e452
jgeogy <josephgeogy@gmail.com>
2021-07-12 15:35:41 +0530

    Logs added

--- assets/scenes/Preload.js
+++ assets/scenes/Preload.js
@@ -25,19 +25,27 @@ class Preload extends Phaser.Scene {
 
 			this.loadingText.text = "Loading " + p * 100 + "%"
 			if(p == 1) {
-				this.loadingText.text = "loading completed progress";
-				//this.scene.start("Menu", {fromLoad: 1, levelNum: 1, levelDiff: 2});
+				//this.loadingText.text = p;
 			}
 		});
 
-		this.load.on(Phaser.Loader.Events.COMPLETE, () => {
-			this.loadingText.text = "loading completed complete";
+		this.load.on('complete', () => {
+			//this.loadingText.text = "loading completed progress";
 			//this.scene.start("Menu", {fromLoad: 1});
 			//this.scene.start("Menu", {fromLoad: 1, levelNum: 1, levelDiff: 2});
+			this.logText.text += "||complete called";
 		});
 
-		this.load.on(Phaser.Loader.Events.FILE_LOAD_ERROR, f => {
+		this.load.on('loaderror', f => {
 			this.errorText.text += f;
+		});
+
+		this.load.on('postprocess', () => {
+			this.logText.text += "||post process called";
+		});
+
+		this.load.on('filecomplete', f => {
+			this.logText.text += " " + f;
 		});
 	}
 
@@ -45,13 +53,10 @@ class Preload extends Phaser.Scene {
 		
 		this.load.pack("asset-pack", "assets/asset-pack.json");
 		this.load.pack("asset-pack_Game", "assets/images/Game/asset-pack_Game.json");
-		//this.load.pack("ap_Audio", "assets/audio/asset-pack_audio.json");
+		this.load.pack("ap_Audio", "assets/audio/asset-pack_audio.json");
 	}
 	
 	_create() {
-		
-		// image
-		this.add.image(604, 323, "dino");
 		
 		// loadingText
 		const loadingText = this.add.text(600, 375, "", {});
@@ -62,10 +67,18 @@ class Preload extends Phaser.Scene {
 		const errorText = this.add.text(100, 50, "", {});
 		errorText.setOrigin(0, 0.5);
 		errorText.text = "Errors: ";
-		errorText.setStyle({"fontSize":"48px"});
+		errorText.setStyle({"fontSize":"24px"});
 		
+		const logText = this.add.text(100, 150, "", {});
+		logText.setOrigin(0, 0);
+		logText.text = "Log: ";
+		logText.setStyle({"fontSize":"12px"});
+
 		this.loadingText = loadingText;
 		this.errorText = errorText;
+		this.logText = logText;
+
+		this.logText.setWordWrapWidth(500);
 	}
 
 	/* END-USER-CODE */
