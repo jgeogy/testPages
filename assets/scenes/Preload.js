024280a16fd618dc8b3fcf3230f2ec42953af396
jgeogy <josephgeogy@gmail.com>
2021-07-13 11:37:10 +0530

    VolcanoGame

--- assets/scenes/Preload.js
+++ assets/scenes/Preload.js
@@ -1,3 +1,6 @@
+
+// You can write more code here
+
 /* START OF COMPILED CODE */
 
 class Preload extends Phaser.Scene {
@@ -10,8 +13,27 @@ class Preload extends Phaser.Scene {
 		/* END-USER-CTR-CODE */
 	}
 	
-	editorCreate() {
+	_preload() {
+		
+		this.load.pack("asset-pack", "assets/asset-pack.json");
 	}
+	
+	_create() {
+		
+		// image
+		this.add.image(604, 323, "dino");
+		
+		// loadingText
+		const loadingText = this.add.text(600, 375, "", {});
+		loadingText.setOrigin(0.5, 0.5);
+		loadingText.text = "Loading";
+		loadingText.setStyle({"fontSize":"48px"});
+		
+		this.loadingText = loadingText;
+	}
+	
+	/** @type {Phaser.GameObjects.Text} */
+	loadingText;
 	
 	/* START-USER-CODE */
 
@@ -23,69 +45,14 @@ class Preload extends Phaser.Scene {
 
 		this.load.on(Phaser.Loader.Events.PROGRESS, p => {
 
-			this.loadingText.text = "Loading " + p * 100 + "%"
-			if(p == 1) {
-				//this.loadingText.text = p;
-			}
+			this.loadingText.text = "Loading " + Math.floor(p * 100) + "%"
 		});
 
-		this.load.on('complete', () => {
-			//this.loadingText.text = "loading completed progress";
-			//this.scene.start("Menu", {fromLoad: 1});
-			//this.scene.start("Menu", {fromLoad: 1, levelNum: 1, levelDiff: 2});
-			this.logText.text += "||complete called";
-		});
+		this.load.on(Phaser.Loader.Events.COMPLETE, () => {
 
-		this.load.on('loaderror', f => {
-			this.errorText.text += f;
-		});
-
-		this.load.on('postprocess', () => {
-			this.logText.text += "||post process called";
-		});
-
-		this.load.on('filecomplete', f => {
-			this.logText.text += " " + f;
+			this.scene.start("Level");
 		});
 	}
-
-	_preload() {
-		
-		this.load.pack("asset-pack", "assets/asset-pack.json");
-		this.load.pack("asset-pack_Game", "assets/images/Game/asset-pack_Game.json");
-		this.load.pack("ap_Audio", "assets/audio/asset-pack_audio.json");
-	}
-	
-	_create() {
-		
-		// loadingText
-		const loadingText = this.add.text(600, 375, "", {});
-		loadingText.setOrigin(0.5, 0.5);
-		loadingText.text = "Loading";
-		loadingText.setStyle({"fontSize":"48px"});
-
-		const errorText = this.add.text(100, 50, "", {});
-		errorText.setOrigin(0, 0.5);
-		errorText.text = "Errors: ";
-		errorText.setStyle({"fontSize":"24px"});
-		
-		const logText = this.add.text(100, 150, "", {});
-		logText.setOrigin(0, 0);
-		logText.text = "Log: ";
-		logText.setStyle({"fontSize":"12px"});
-
-		this.loadingText = loadingText;
-		this.errorText = errorText;
-		this.logText = logText;
-
-		this.logText.setWordWrapWidth(500);
-
-		if(this.sys.game.device.os.iOS) {
-			this.logText.text += " io";
-			this.logText.text += this.sys.game.device.os.iOSVersion;
-		}
-	}
-
 	/* END-USER-CODE */
 }
 
