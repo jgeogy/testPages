f2cbfed4d34a7eb280abba891610877d8a4cb027
jgeogy <josephgeogy@gmail.com>
2021-07-14 17:45:39 +0530

    Assets changed

--- Load.js
+++ Load.js
@@ -13,11 +13,9 @@ class Load extends Phaser.Scene {
 
     this.loadTxt = loadTxt;
 
-    this.load.image('LoadBG', 'assets/images/load/bg-8.png');
-    this.load.image('LoadIndicator', 'assets/images/load/indicator-8.png');
-    this.load.image('LoadBar', 'assets/images/load/bg-8.png');
-    this.load.image('LoadFill', 'assets/images/load/bg-8.png');
-    this.load.image('CSBG', 'assets/images/cutscene/CUTSCENES_WORLDOFNUMBERS.png');
+    this.load.image('LoadBG', 'assets/images/load/bg.png');
+    this.load.image('LoadIndicator', 'assets/images/load/dino.png');
+    this.load.image('CSBG', 'assets/images/cutscene/csbg.png');
 
     this.load.on(Phaser.Loader.Events.PROGRESS, p => {
 			this.loadTxt.text = "Loading " + p * 100 + "%"
