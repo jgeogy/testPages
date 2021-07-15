c36f9eb6cabdf53820e4c69d4c378aa1a6e8ab98
jgeogy <josephgeogy@gmail.com>
2021-07-15 13:09:52 +0530

    CodeGame commit 1
    game without editor

---
+++ Cutscene.js
@@ -0,0 +1,27 @@
+class Cutscene extends Phaser.Scene {
+  constructor() {
+    super({key: "Cutscene"});
+
+    this.levelNum;
+    this.levelDiff;
+  }
+
+  //VARIABLES
+
+
+  init(data) {
+    this.levelNum = data.levelNum;
+    this.levelDiff = data.levelDiff;
+  }
+
+  create() {
+    this.add.image(640,384, 'CSBG');
+
+    this.time.delayedCall(2000, this.timerEnd, null, this);
+  }
+
+  timerEnd() {
+    this.scene.start("Menu", {fromLoad: 0, levelNum: this.levelNum, levelDiff: this.levelDiff});
+  }
+
+}
