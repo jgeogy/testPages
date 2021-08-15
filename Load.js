1bb9c029a275f2324e779198f58b1b1e7d31a2df
jgeogy <josephgeogy@gmail.com>
2021-08-15 17:10:20 +0530

    CO_Test commit 10
    changed from ES6 to ES5 using babeljs.io

--- Load.js
+++ Load.js
@@ -1,41 +1,78 @@
 "use strict";
-class Load extends Phaser.Scene {
-  constructor() {
-    super({key: "Load"});
 
-    this.loadTxt;
-  }
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-  preload() {
-    const loadTxt = this.add.text(600, 375, "", {});
-    loadTxt.setOrigin(0.5, 0.5);
-    loadTxt.text = "Loading";
-    loadTxt.setStyle({"fontSize":"48px"});
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    this.loadTxt = loadTxt;
-    var myScene = this.scene;
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-    this.load.image('LoadBG', 'assets/images/load/bg.png');
-    this.load.image('LoadIndicator', 'assets/images/load/dino.png');
-    this.load.image('CSBG', 'assets/images/cutscene/csbg.png');
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-    this.load.on('progress', function (p) {
-			loadTxt.text = "Loading " + p * 100 + "%";
-		});
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
-    this.load.on('complete', function () {
-      myScene.start("Menu", {fromLoad: 1, levelNum: 1, levelDiff: 2});
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var Load = /*#__PURE__*/function (_Phaser$Scene) {
+  _inherits(Load, _Phaser$Scene);
+
+  var _super = _createSuper(Load);
+
+  function Load() {
+    var _this;
+
+    _classCallCheck(this, Load);
+
+    _this = _super.call(this, {
+      key: "Load"
     });
-
+    _this.loadTxt;
+    return _this;
   }
 
-  init(data) {
+  _createClass(Load, [{
+    key: "preload",
+    value: function preload() {
+      var loadTxt = this.add.text(600, 375, "", {});
+      loadTxt.setOrigin(0.5, 0.5);
+      loadTxt.text = "Loading";
+      loadTxt.setStyle({
+        "fontSize": "48px"
+      });
+      this.loadTxt = loadTxt;
+      var myScene = this.scene;
+      this.load.image('LoadBG', 'assets/images/load/bg.png');
+      this.load.image('LoadIndicator', 'assets/images/load/dino.png');
+      this.load.image('CSBG', 'assets/images/cutscene/csbg.png');
+      this.load.on('progress', function (p) {
+        loadTxt.text = "Loading " + p * 100 + "%";
+      });
+      this.load.on('complete', function () {
+        myScene.start("Menu", {
+          fromLoad: 1,
+          levelNum: 1,
+          levelDiff: 2
+        });
+      });
+    }
+  }, {
+    key: "init",
+    value: function init(data) {}
+  }, {
+    key: "create",
+    value: function create() {}
+  }]);
 
-  }
-
-  create() {
-
-  }
-
-}
+  return Load;
+}(Phaser.Scene);
