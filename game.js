4f2e2326d8e632a6e2896ea17006cb681d35580f
jgeogy <josephgeogy@gmail.com>
2021-08-22 11:28:35 +0530

    CO_TestFull commit 4
    ogg files added

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
-"use strict";
-
 var config = {
   type: Phaser.AUTO,
   width: 1280,
   height: 768,
   backgroundColor: "#555555",
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  },
-  input: {
-    activePointers: 1
-  },
-  scene: [Load, Menu, CutScene, GameScene, GameOver]
-};
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH
+	},
+	input: {
+		activePointers: 1
+	},
+  scene: [ Load, Menu, CutScene, GameScene, GameOver ]
+
+}
+
 var game = new Phaser.Game(config);
