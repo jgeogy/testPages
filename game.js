aa0ee1e5a3cefd6b2fd56afbdc442ed210f4dca5
jgeogy <josephgeogy@gmail.com>
2021-09-06 16:07:50 +0530

    CO_TestFull Howler ES5
    ES5 version test

--- game.js
+++ game.js
@@ -1,20 +1,20 @@
+"use strict";
+
 var config = {
   type: Phaser.AUTO,
   width: 1280,
   height: 768,
   backgroundColor: "#555555",
-	scale: {
-		mode: Phaser.Scale.FIT,
-		autoCenter: Phaser.Scale.CENTER_BOTH
-	},
-	input: {
-		activePointers: 1
-	},
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  input: {
+    activePointers: 1
+  },
   audio: {
     disableWebAudio: true
   },
-  scene: [ Load, Menu, CutScene, GameScene, GameOver ]
-
-}
-
+  scene: [Load, Menu, CutScene, GameScene, GameOver]
+};
 var game = new Phaser.Game(config);
